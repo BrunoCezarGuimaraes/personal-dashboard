023018f14b63a4fcef86a0ff89854c8367c312ce
Bruno Cezar Guimarães <bruno_guimaraes007@hotmail.com>
2021-11-01 13:38:50 -0300

    Norte service

---
+++ src/app/shared/note.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Note } from './note.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  notes: Note[] | undefined;
+
+  constructor() { }
+
+  getNotes() {
+    return this.notes
+  }
+
+  getNote(id: string) {
+    //Retorna True quando o n.id é igual ao id passado
+    return this.notes?.find(n => n.id === id)
+  }
+
+  addNote(note: Note) {
+    this.notes?.push(note)
+  }
+
+  updateNote(id: string, updateFields: Partial<Note>) {
+    const note = this.getNote(id)
+    Object.assign(note, updateFields)
+  }
+
+  deleteNote(id: string) {
+    const noteIndex = this.notes?.findIndex(n => n.id ===id)
+    if (noteIndex == -1) return this.notes?.splice(noteIndex, 1)
+  }
+}
